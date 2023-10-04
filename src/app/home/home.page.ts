8f9d7826c652bf01e122157485c62b61c2b388ec
MatheusAbreuLz <mcurveloluz1@gmail.com>
2023-10-04 18:19:37 -0300

    SEARCH BAR

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,27 +9,114 @@ export class HomePage {
 
   isLoading: boolean = false;
   funcionarios: any;
+  IsModalOpen: boolean = false;
+  radio: any = 'all';
 
   constructor() {
     this.getFuncionarios();
   }
 
   input: any = {
-    'nome':'',
-    'sobrenome':'',
-    'cargo':'',
-    'datanasc':'',
-    'endereco':'',
-    'cidade':'',
-    'cep':'',
-    'pais':'',
-    'fone':'',
-    'salario':'',
-    'id':''
+    'Nome':'',
+    'Sobrenome':'',
+    'Cargo':'',
+    'Datanasc':'',
+    'Endereco':'',
+    'Cidade':'',
+    'CEP':'',
+    'Pais':'',
+    'Fone':'',
+    'Salario':'',
+    'CodFun':''
   };
 
-  envia(){
+  searched: any  = {
+    'pes':''
+  }
 
+  atualizar(id: any){
+    fetch('http://localhost/api/funcionario/listar_funcionario_id.php', {
+      method:'post',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body:JSON.stringify({
+        'pes':id
+      })
+    }).then(response => response.json())
+    .then(response => {
+      this.input = response['funcionarios'][0]
+      console.log(this.input)
+      this.setOpen(true);
+    }).catch(erro => {
+      console.log(erro);
+    })
+  }
+  setOpen(OPEN: any){
+    this.IsModalOpen = OPEN;
+    if(OPEN = false){
+      this.clean()
+    }
+  }
+  clean(){
+    this.input = {
+      'Nome':'',
+      'Sobrenome':'',
+      'Cargo':'',
+      'Datanasc':'',
+      'Endereco':'',
+      'Cidade':'',
+      'CEP':'',
+      'Pais':'',
+      'Fone':'',
+      'Salario':'',
+      'CodFun':''
+    };
+  }
+
+  search(){
+    fetch('http://localhost/api/funcionario/listar_funcionario_'+this.radio+'.php', {
+      method:'post',
+      headers: {
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(this.searched)
+    }).then(response => response.json())
+    .then(response =>{
+      console.log(response);
+      this.funcionarios = response.funcionarios
+    }).catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{
+    })
+  }
+
+  sendAtualiza(){
+    fetch('http://localhost/api/funcionario/atualizar_funcionario.php',{
+      method:'post',
+      headers: {
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(this.input)
+    }).then(response => response.json())
+    .then(response =>{
+      console.log(response);
+    }).catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{
+      this.setOpen(false)
+      if(this.searched.pes == ''){
+        this.getFuncionarios()
+      } else {
+        this.search()
+      }
+      this.clean()
+    })
+  }
+
+  envia(){
     fetch('http://localhost/api/funcionario/inserir_funcionario.php', {
       method:'post',
       headers: {
@@ -43,7 +130,12 @@ export class HomePage {
       console.log(erro);
     })
     .finally(()=>{
-      console.log('vai la ve')
+      this.clean()
+      if(this.searched.pes == ''){
+        this.getFuncionarios()
+      } else {
+        this.search()
+      }
     })
   }
 
